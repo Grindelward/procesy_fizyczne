42bc60bd1d74f2a5253832669b1e873350d51290
Sebastian Bachorczyk <Sebastian Bachorczyk>
2016-03-30 19:11:08 +0200

    Init

---
+++ Procesy Fizyczne/Assets/RandomTerrain.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomTerrain : MonoBehaviour {
+
+    [Header("Tiles Random")]
+    public float tiling = 10.0f;
+    public Terrain terrain;
+
+    // Use this for initialization
+    void Start () {
+        GenerateHeights(terrain, tiling);
+    }
+
+    public void GenerateHeights(Terrain terrain, float tileSize)
+    {
+        float[,] heights = new float[terrain.terrainData.heightmapWidth, terrain.terrainData.heightmapHeight];
+
+        for (int i = 0; i < terrain.terrainData.heightmapWidth; i++)
+        {
+            for (int k = 0; k < terrain.terrainData.heightmapHeight; k++)
+            {
+                heights[i, k] = Mathf.PerlinNoise(((float)i / (float)terrain.terrainData.heightmapWidth) * tileSize, ((float)k / (float)terrain.terrainData.heightmapHeight) * tileSize) / 20.0f;
+            }
+        }
+        terrain.terrainData.SetHeights(0, 0, heights);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Procesy Fizyczne/Assets/move.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class move : MonoBehaviour {
+
+    public Rigidbody rb;
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () { // before rendering a frame
+	    
+	}
+
+    void FixedUpdate() // before physics code
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        movement = Random.insideUnitSphere * speed;
+        movement.y = 0.0f;
+
+        rb.AddForce(movement * speed);
+    }
+}
